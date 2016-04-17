9c58e74ae6a4edb80a12e57bd52a9c46152aa422
loveincloud <2587687121@qq.com>
2016-04-17 12:25:28 +0800

    #1 #58 实验1

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903215/rebootpc/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903215.rebootpc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
